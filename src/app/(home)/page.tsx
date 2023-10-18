89b430999fa2bcfba209b2098d5d2ff99beb3e59
Eduardo Castro <eduardo.castro@g.globo>
2023-10-17 22:11:52 -0300

    feat: add mouse section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -20,6 +20,13 @@ export default async function Home() {
       },
     },
   });
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
 
   return (
     <div className="flex flex-col gap-8">
@@ -51,6 +58,11 @@ export default async function Home() {
         src="/banner-fones.png"
         alt="Até 55% de desconto em mouses!"
       />
+
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
+      </div>
     </div>
   );
 }
