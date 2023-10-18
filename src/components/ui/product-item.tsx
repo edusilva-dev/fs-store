3b66c3f7384ac9bacefa69408b31e0cbfb76d48a
Eduardo Castro <eduardo.castro@g.globo>
2023-10-18 20:15:55 -0300

    feat: ensure user goes to product detail page on product click

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -2,6 +2,7 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDownIcon } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
@@ -9,46 +10,48 @@ interface ProductItemProps {
 
 const ProducItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex h-full w-full flex-col gap-4">
-      <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
-        {product.discountPercentage > 0 && (
-          <Badge className="absolute left-2 top-2 px-2 py-[2px]">
-            <ArrowDownIcon size={14} />
-            {product.discountPercentage}%
-          </Badge>
-        )}
+    <Link href={`/product/${product.slug}`}>
+      <div className="flex h-full w-full flex-col gap-4">
+        <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
+          {product.discountPercentage > 0 && (
+            <Badge className="absolute left-2 top-2 px-2 py-[2px]">
+              <ArrowDownIcon size={14} />
+              {product.discountPercentage}%
+            </Badge>
+          )}
 
-        <Image
-          src={product.imageUrls[0]}
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          style={{
-            objectFit: "contain",
-          }}
-          alt={product.name}
-        />
-      </div>
+          <Image
+            src={product.imageUrls[0]}
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            style={{
+              objectFit: "contain",
+            }}
+            alt={product.name}
+          />
+        </div>
 
-      <div className="flex flex-col gap-1">
-        <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-          {product.name}
-        </p>
-
-        <div className="flex items-center gap-2">
-          <p className="text-base font-semibold">
-            R$ {product.totalPrice.toFixed(2)}
+        <div className="flex flex-col gap-1">
+          <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+            {product.name}
           </p>
 
-          {product.discountPercentage > 0 && (
-            <p className="text-xs line-through opacity-75">
-              R$ {Number(product.basePrice).toFixed(2)}
+          <div className="flex items-center gap-2">
+            <p className="text-base font-semibold">
+              R$ {product.totalPrice.toFixed(2)}
             </p>
-          )}
+
+            {product.discountPercentage > 0 && (
+              <p className="text-xs line-through opacity-75">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
