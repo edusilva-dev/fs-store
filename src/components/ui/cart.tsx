89b50a2da6ea61076723c905f18a8ef46eaed507
Eduardo Castro <eduardo.castro@g.globo>
2023-10-19 23:38:11 -0300

    fix: ensure cart total and subtotal are correctly calculated

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -53,7 +53,7 @@ const Cart = () => {
 
         <div className="flex items-center justify-between text-xs">
           <p>Descontos</p>
-          <p>R$ {totalDiscount.toFixed(2)}</p>
+          <p>- R$ {totalDiscount.toFixed(2)}</p>
         </div>
 
         <Separator />
